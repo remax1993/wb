cc8675f1f9d5a1e5e50eca0490caa801d02b6c12
remax1993 <remax1993@gmail.com>
2019-07-23 15:03:32 +0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatSortModule, MatTableModule} from "@angular/material";
+import { StartPageComponent } from './start-page/start-page.component';
+import { TabeComponent } from './tabe/tabe.component';
+import {OverlayModule} from "@angular/cdk/overlay";
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    TabeComponent,
+    RxjsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    OverlayModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [TabeComponent]
 })
 export class AppModule { }
